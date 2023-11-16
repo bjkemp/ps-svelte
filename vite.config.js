a54458970de2d7f896478d27defc21eb859ea526
Benjamin Kemp <bjkemp@gmail.com>
2023-11-16 12:16:39 -0500

    Add rollup options for Button component

--- vite.config.js
+++ vite.config.js
@@ -9,12 +9,29 @@ export default defineConfig({
       // enable run-time checks when not in production
       dev: true,
     }
-  })],
+  })
+  ],
   build: {
     target: 'esnext',
     outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true,
     minify: true,
+    rollupOptions: {
+      input: 'src/components/Button.svelte',
+      output: {
+        format: 'es',
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+        manualChunks: undefined,
+        exports: 'named',
+        sourcemap: true,
+        sourcemapExcludeSources: false,
+        inlineDynamicImports: false,
+        plugins: [],
+      },
+      plugins: [],
+    }
   },
 })
