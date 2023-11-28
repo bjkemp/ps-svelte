713bf9925fddadad9e5dc3125610d42cb0eef5a0
Benjamin Kemp <bjkemp@gmail.com>
2023-11-28 08:31:48 -0500

    Add md5 dependency to package.json and pnpm-lock.yaml

--- src/components/TeacherPortalSectionFilter.svelte
+++ src/components/TeacherPortalSectionFilter.svelte
@@ -1,29 +1,66 @@
 <svelte:options customElement="ps-svelte-teacher-portal-section-filter" />
 <script>
   import { onMount } from 'svelte';
+  import md5 from 'md5';
   import devSections from '$ps/teachers/json/section.json';
 
   let sections = [];
+  let filters = new Set(['Hey']); // Use a Set to store unique values
 
-  onMount(async () => {
-    const isProduction = false /* Add your logic here to check if the environment is production */;
+  async function loadSections() {
+    const isProduction = false; /* Add your logic here to check if the environment is production */
     if (isProduction) {
       const response = await fetch('/teachers/json/section.json');
       sections = await response.json();
     } else {
       sections = devSections;
     }
+  }
+
+  function processSection(section) {
+    let not_in_session = false;
+    
+    const this_filters = [
+      `Course: ${section.COURSE_NAME}`,
+      `Exp: ${section.EXTERNAL_EXPRESSION}`,
+      `Term: ${section.TERM}`,
+      `Period: ${section.EXTERNAL_EXPRESSION.split('(')[0]}`,
+      `Days: ${section.EXTERNAL_EXPRESSION.split('(')[1].replaceAll(')', '')}`,
+      `${section.ACTIVE == 1 ? 'Term: Active' : 'Inactive'}`,
+      `Meets Today: ${not_in_session ? 'No' : 'Yes'}`
+    ];
+
+    this_filters.forEach((filter) => {
+      filters = new Set([...filters, filter]); // Use a new Set to trigger reactivity
+    });
+  }
+
+  onMount(async () => {
+    await loadSections();
+    sections.forEach(processSection);
+    filters = new Set([...Array.from(filters).sort((a, b) => {
+      if (a.includes('Meets Today:')) return -1;
+      if (a < b) return -1;
+      if (a > b) return 1;
+      return 0;
+    })]);
   });
 </script>
 
-<select name="sections" id="sections">
-  {#each sections as section}
-    <option value={section.ID}>{section.COURSE_NAME}</option>
-  {/each}
+<select name="section_filters" id="section_filters">
+  {#if filters.size > 0}
+    {#each Array.from(filters) as filter, index (index)}
+      <option value={md5(filter)}>{filter}</option>
+    {/each}
+  {:else}
+    <option value="">No filters available</option>
+  {/if}
 </select>
 
+<pre>{Array.from(filters)}</pre>
+
 <style>
-  #sections {
+  #section_filters {
     margin: 0 0 0 10px;
     padding: 0;
     height: 30px;
@@ -33,6 +70,5 @@
     background-color: #fff;
     border: 1px solid #000;
     border-radius: 5px;
-  
   }
 </style>
